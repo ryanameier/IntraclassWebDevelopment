bb0260205e40794c094b194db7e3e06ef50d541c
ryanameier <rymei10@gmail.com>
2016-08-05 14:20:53 -0700

    test

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,4 +2,28 @@ class IndexController < ApplicationController
   def index
     @contact = ContactUs::Contact.new
   end
+
+  def slideshow
+
+  end
+
+  def video
+
+  end
+
+  def image
+
+  end
+
+  def slideshow2
+
+  end
+
+  def video2
+
+  end
+
+  def image2
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'index#index'
+  
+  get '/index/slideshow', to: 'index#slideshow'
+  get '/index/video', to: 'index#video'
+  get '/index/image', to: 'index#image'
+  get '/index/slideshow2', to: 'index#slideshow2'
+  get '/index/video2', to: 'index#video2'
+  get '/index/image2', to: 'index#image2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
