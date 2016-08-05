3ee4ee282efaa6341152e04cb1cc2b8ef6568ca2
ryanameier <rymei10@gmail.com>
2016-08-05 15:45:49 -0700

    test

--- app/assets/javascripts/javascripts/custom.js
+++ app/assets/javascripts/javascripts/custom.js
@@ -220,7 +220,7 @@
 			styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
 		});
 
-		var image = new google.maps.MarkerImage('assets/images/map-icon.png',
+		var image = new google.maps.MarkerImage('/assets/map-icon.png',
 			new google.maps.Size(80, 80),
 			new google.maps.Point(0, 0),
 			new google.maps.Point(40, 40)
